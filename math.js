b4aa968eb452fb8ed95023d6985ebf20532070af
dhirajkrgupta <157622113+dhirajkrgupta@users.noreply.github.com>
2025-11-07 15:40:24 +0530

    done math.js

--- math.js
+++ math.js
@@ -1,11 +1,11 @@
-function isPointInCircle(point, circle, radius) {
+export function isPointInCircle(point, circle, radius) {
   const dx = point.x - circle.x;
   const dy = point.y - circle.y;
   return dx * dx + dy * dy < radius * radius;
 }
 
 //B(t) = (1−t)³P₀ + 3(1−t)²tP₁ + 3(1−t)t²P₂ + t³P₃
-function cubicBezier(t, p0, p1, p2, p3) {
+export function cubicBezier(t, p0, p1, p2, p3) {
   const c0 = Math.pow(1 - t, 3);
   const c1 = 3 * Math.pow(1 - t, 2) * t;
   const c2 = 3 * (1 - t) * Math.pow(t, 2);
@@ -17,7 +17,7 @@ function cubicBezier(t, p0, p1, p2, p3) {
 }
 
 //B'(t) = 3(1−t)²(P₁−P₀) + 6(1−t)t(P₂−P₁) + 3t²(P₃−P₂)
-function slope(t, p0, p1, p2, p3) {
+export function slope(t, p0, p1, p2, p3) {
   const c0 = 3 * Math.pow(1 - t, 2);
   const c1 = 6 * (1 - t) * t;
   const c2 = 3 * Math.pow(t, 2);
