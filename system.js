964702cbb5ddb112ef47740c4a06c81811d72e05
dhirajkrgupta <157622113+dhirajkrgupta@users.noreply.github.com>
2025-11-07 15:59:46 +0530

    refactored update method for updating the physics of movable handle points

--- system.js
+++ system.js
@@ -1,24 +1,36 @@
-class BezierSystem{
-    constructor(){
-
+class BezierSystem {
+    constructor(basepositions, config) {
+        this.p0 = basepositions.p0;
+        this.p1 = basepositions.p1;
+        this.p2 = basepositions.p2;
+        this.p3 = basepositions.p3;
+        this.k = config.k;
+        this.damping = config.damping;
+        this.showtangents = config.showtangents;
+        this.tx = null;
+        this.ty = null;
+        this.vx = 0;
+        this.vy = 0;
     }
 
-    update(){
+    update(dt) {
+        [this.p1, this.p2].forEach((point) => {
+        // acceleration = -k * (position - target) - damping * velocity
+        const ax = -this.k * (point.x - point.tx) - this.damping * point.vx;
+        const ay = -this.k * (point.y - point.ty) - this.damping * point.vy;
 
-    }
-    resize(scaleFactor){
+        // v=v+at
+        point.vx += ax * dt;
+        point.vy += ay * dt;
 
+        // s = s + vt
+        point.x += point.vx * dt;
+        point.y += point.vy * dt;
+        });
     }
-    setTarget(point, x, y){
-
-    }
-    setParams(k, damping){
-
-    }
-    toggleTangents(show){
-
-    }
-    getPoints(){
-        
-    }
+    resize(scaleFactor) {}
+    setTarget(point, x, y) {}
+    setParams(k, damping) {}
+    toggleTangents(show) {}
+    getPoints() {}
 }
